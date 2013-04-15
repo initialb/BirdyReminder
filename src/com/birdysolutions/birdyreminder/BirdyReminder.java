eb80b9aca8b6545a440f5d68d4589ecba6ddc294
initiab <initialb@gmail.com>
2013-04-15 18:46:45 +0800

    Add main class.

---
+++ src/com/birdysolutions/birdyreminder/BirdyReminder.java
@@ -0,0 +1,13 @@
+package com.birdysolutions.birdyreminder;
+
+public class BirdyReminder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
