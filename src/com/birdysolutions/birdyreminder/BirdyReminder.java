b7a064a67a238925a85e4770e73fe31832bdc986
InitialB <initialb@gmail.com>
2013-04-17 23:54:49 +0800

    construct a rough frame.

--- src/com/birdysolutions/birdyreminder/BirdyReminder.java
+++ src/com/birdysolutions/birdyreminder/BirdyReminder.java
@@ -1,14 +1,37 @@
 package com.birdysolutions.birdyreminder;
 
+import javax.swing.*;
+import javax.swing.JFrame;
+
 public class BirdyReminder {
 
+	
+	JFrame f = new JFrame("BirdyReminder");
+	JTextField time = new JTextField(10);
+	JButton ok = new JButton("set");
+	
+	public void init()
+	{
+		JPanel bottom = new JPanel();
+		bottom.add(time);
+		bottom.add(ok);
+
+		f.add(bottom);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		f.pack();
+		f.setVisible(true);
+	}
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int iInterval = 1;
+		new BirdyReminder().init();
 	}
 
 }
