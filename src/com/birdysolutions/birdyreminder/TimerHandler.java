7167660899235b880c3ba830318bfbedc6f249d0
InitialB <initialb@gmail.com>
2013-04-24 22:36:04 +0800

    add timer and timer task

--- src/com/birdysolutions/birdyreminder/TimerHandler.java
+++ src/com/birdysolutions/birdyreminder/TimerHandler.java
@@ -1,5 +1,26 @@
 package com.birdysolutions.birdyreminder;
 
-public class TimerHandler {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class TimerHandler {   
+   
+    public static void main(String[] args) { 
+    	TimerHandler tTask=new TimerHandler();
+        tTask.timeVoid();
+    }
+    
+    public void timeVoid(){
+        final Timer timer = new Timer();
+        TimerTask tt=new TimerTask() { 
+            @Override
+            public void run() {
+                System.out.println("µΩµ„¿≤£°");
+                timer.cancel();
+            }
+        };
+
+        System.out.println("begin...");
+        timer.schedule(tt, 3000);
+    }
 }
